73aa89602d3421241597e9ad2be78b8c51ba149e
Melanie98 <melanie_bal@hotmail.com>
2017-12-20 09:31:10 +0100

    Aanpassingen en samenvoegen

--- dashboard.php
+++ dashboard.php
@@ -10,5 +10,9 @@
         <a href="pages/tournooi/createTournooi.php">Aanmaken tournooi</a>
         <a href="pages/participate/createParticipate.php">Inschrijven voor tournooi</a>
         <a href="pages/application/createApplication.php">Inschrijven voor kerstontbijt</a>
+        <a href="pages/users/overviewUsers.php">Overzicht Gebruikers</a>
+        <a href="pages/tournooi/overviewTournooi.php">Overzicht Toernooien</a>
+        <a href="pages/participate/overviewParticipant.php">Overzicht Aanmeldingen</a>
+        <a href="pages/application/overviewPayment.php">Betalings Overzicht</a>
     </body>
 </html>
