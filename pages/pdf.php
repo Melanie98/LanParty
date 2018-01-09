da6cac61384609146c3a64208915cc7243961592
anoukhermsen <al.hermsen@hotmail.com>
2018-01-09 11:07:43 +0100

    PDF

--- pages/pdf.php
+++ pages/pdf.php
@@ -1,9 +1,3 @@
-<html>
-    <head>
-        <title>Factuur</title>
-        <meta charset="utf-8">
-    </head>
-</html>
 <?php
 
     require('../class/fpdf.php');
@@ -13,34 +7,35 @@
 
 
     session_start();
-    $_SESSION['winkelmandje'];
 
-    $db = new DBHelper();
+
 
 
     $pdf = new FPDF();
     $pdf->AddPage();
     //$pdf->Image(URL::to('/').'/img/logo.jpg',19,20,35);
     define('EURO',chr(128));
-    $pdf->SetFont('Arial', '', 12);
+    $pdf->SetFont('Arial', 'B', 20);
+    $pdf->Cell(40,10,'Factuur lanparty');
     $pdf->SetY('70');
     $pdf->SetX('19');
-    $pdf->Cell(90, 25, 'Naam: '.$cusName);
+    $pdf->SetFont('Arial', '', 12);
+    $pdf->Cell(90, 25, 'Naam: '/*.$cusName*/);
     $pdf->SetY('100');
     $pdf->SetX('19');
 
     //De cell is gewoon een div waar je als eerste aangeeft 90 breed 25 lang
-    $pdf->Cell(90, 25, 'Factuurdatum: '.$var['orderDate']);
+    $pdf->Cell(90, 25, 'Factuurdatum: '/*.$var['orderDate']);
     $pdf->SetY('110');
     $pdf->SetX('19');
     $pdf->SetY('140');
     $pdf->SetX('20');
-    $pdf->Cell(90, 25, 'Productnaam: '.$var['productName']);
+    $pdf->Cell(90, 25, 'Productnaam: '/*.$var['productName']);
     $pdf->SetX('60');
-    $pdf->Cell(90, 25, 'Product beschrijving: '. $var['productDesc']);
+    $pdf->Cell(90, 25, 'Product beschrijving: '/*. $var['productDesc']*/);
     $pdf->SetX('100');
     $pdf->SetX('140');
-    $pdf->Cell(90, 25, 'Stukprijs: '.$var['totalAmount']);
+    $pdf->Cell(90, 25, 'Stukprijs: '/*.$var['totalAmount']*/);
     $pdf->SetX('170');
     $pdf->Cell(90, 25, 'Bedrag excl BTW: ');
     $pdf->SetY('150');
